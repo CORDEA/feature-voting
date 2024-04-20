df07c7fe06128214ff157523447aa7eae0e2d379
Yoshihiro Tanaka <contact@cordea.jp>
2024-04-20 14:06:34 +0900

    fetch features

--- src/entry.py
+++ src/entry.py
@@ -4,19 +4,23 @@ from urllib.parse import urlparse
 from js import Response
 
 
-def on_get_posts():
-    return Response.new('get_posts')
+async def on_get_posts(env):
+    result = await env.DB.prepare('SELECT * FROM Features').all()
+    return Response.json(result.results)
 
 
-def on_get_post(id):
-    return Response.new("get_post %s" % id)
+async def on_get_post(env, id):
+    result = await env.DB.prepare('SELECT * FROM Features WHERE ID = ?1').bind(id).all()
+    if len(result.results) > 0:
+        return Response.json(result.results[0])
+    return Response.json({}, status=404)
 
 
-def on_post_new():
+def on_post_new(env):
     return Response.new('post_new')
 
 
-def on_vote(id):
+def on_vote(env, id):
     return Response.new('post_new %s' % id)
 
 
@@ -26,14 +30,14 @@ async def on_fetch(request, env):
     queries = urlparse(request.url).query
     if method == "GET":
         if path == "/posts":
-            return on_get_posts()
+            return on_get_posts(env)
         r = re.fullmatch(r"/posts/(\d+)", path)
         if r is not None:
-            return on_get_post(r.group(1))
+            return on_get_post(env, r.group(1))
     if method == "POST":
         if path == "/posts":
-            return on_post_new()
+            return on_post_new(env)
         r = re.fullmatch(r"/posts/(\d+)/vote", path)
         if r is not None:
-            return on_vote(r.group(1))
+            return on_vote(env, r.group(1))
     return Response.new('Hello, World!')
