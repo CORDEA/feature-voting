1f69cbb1c47e13fac6205b087b607267165c5a24
Yoshihiro Tanaka <contact@cordea.jp>
2024-04-20 14:32:25 +0900

    add a new feature

--- src/entry.py
+++ src/entry.py
@@ -1,7 +1,9 @@
 import re
+from datetime import datetime
 from urllib.parse import urlparse
 
 from js import Response
+from pyodide.ffi import JsException
 
 
 async def on_get_posts(env):
@@ -16,8 +18,26 @@ async def on_get_post(env, id):
     return Response.json({}, status=404)
 
 
-def on_post_new(env):
-    return Response.new('post_new')
+async def on_post_new(env, body):
+    try:
+        created_at = datetime.fromisoformat(body.CreatedAt)
+    except ValueError:
+        return Response.json({}, status=500)
+    try:
+        votes = int(body.Votes)
+    except ValueError:
+        return Response.json({}, status=500)
+    title = body.Title
+    body = body.Body
+    if len(title) <= 0 or len(body) <= 0:
+        return Response.json({}, status=500)
+    result = await env.DB.prepare(
+        'INSERT INTO Features (CreatedAt, Title, Body, Votes) '
+        'VALUES (?1, ?2, ?3, ?4)'
+    ).bind(created_at.isoformat(), title, body, votes).all()
+    if result.success:
+        return Response.json({}, status=201)
+    return Response.json({}, status=500)
 
 
 def on_vote(env, id):
@@ -27,7 +47,6 @@ def on_vote(env, id):
 async def on_fetch(request, env):
     method = request.method
     path = urlparse(request.url).path
-    queries = urlparse(request.url).query
     if method == "GET":
         if path == "/posts":
             return on_get_posts(env)
@@ -36,7 +55,11 @@ async def on_fetch(request, env):
             return on_get_post(env, r.group(1))
     if method == "POST":
         if path == "/posts":
-            return on_post_new(env)
+            try:
+                body = await request.json()
+            except JsException:
+                body = {}
+            return on_post_new(env, body)
         r = re.fullmatch(r"/posts/(\d+)/vote", path)
         if r is not None:
             return on_vote(env, r.group(1))
